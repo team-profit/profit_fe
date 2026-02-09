29d608710e97990f5af4a1db72b185d3891f3adc
j02on <pjylove08@naver.com>
2026-02-09 11:56:05 +0900

    feat : login

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,14 @@
 import { LOGO } from '../assets';
-import { CHATICON } from '../assets';
+// import { CHATICON } from '../assets';
 import { LargeButton } from '../components';
-import { colors, Flex } from '../design-token';
+import { Flex } from '../design-token';
 import styled from '@emotion/styled';
 
 export const MainPage = () => {
   return (
     <Flex justifyContent="center" isColumn alignItems="center" height="100vh">
       <LOGO />
-      <BtnWrapper>
+      {/* <BtnWrapper>
         <LargeButton
           width="100%"
           color={colors.brown[100]}
@@ -17,6 +17,9 @@ export const MainPage = () => {
           <CHATICON />
           카카오로 로그인하기
         </LargeButton>
+      </BtnWrapper> */}
+      <BtnWrapper>
+        <LargeButton width="100%">로그인</LargeButton>
       </BtnWrapper>
     </Flex>
   );
