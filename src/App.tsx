89098f3df646e0551242d8ea90ca87491624be55
j02on <pjylove08@naver.com>
2026-02-13 11:57:30 +0900

    feat : toast 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { GlobalStyle } from './design-token';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
   return (
     <div>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000} // 3초 후 자동 닫힘
+        hideProgressBar={false}
+        newestOnTop={false}
+        theme="colored"
+        closeOnClick
+        pauseOnHover
+        draggable
+        pauseOnFocusLoss
+      />
       <GlobalStyle />
     </div>
   );
